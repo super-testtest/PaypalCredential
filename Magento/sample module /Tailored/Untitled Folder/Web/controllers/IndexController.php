95076475ab8313ffd3cfa74bd1ebf2ec7a763494
Ankit Dobariya <ankit.dobariya@tasolglobal.com>
2017-05-11 12:30:55 +0530

    dkjh

--- Magento/sample module /Tailored/Untitled Folder/Web/controllers/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Company_Web_IndexController extends Mage_Core_Controller_Front_Action
-{
-    public function indexAction()
-    {
-		$this->loadLayout();     
-		$this->renderLayout();
-    }
-}
